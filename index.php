01e651ea0465730240a73e86cf221b29e0c7f706
einnabno <70646362+einnabno@users.noreply.github.com>
2020-09-05 21:44:38 +0800

    Update index.php

--- index.php
+++ index.php
@@ -22,12 +22,12 @@
 			<div class="title" id="bottom-title">PIZZA</div>
 			<br>
 			<ul>
-				<li><a class="in-active" href="index.html">Home</a></li>
-				<li><a href="index.html">About Us</a></li>
-				<li><a href="index.html">Portfolio</a></li>
-				<li><a href="index.html">Clients</a></li>
-				<li><a href="index.html">Gallery</a></li>
-				<li><a href="index.html">Contact Us</a></li>
+				<li><a class="in-active" href="index.php">Home</a></li>
+				<li><a href="index.php">About Us</a></li>
+				<li><a href="index.php">Portfolio</a></li>
+				<li><a href="index.php">Clients</a></li>
+				<li><a href="index.php">Gallery</a></li>
+				<li><a href="index.php">Contact Us</a></li>
 			</ul>
 			<br>
 			<br>
